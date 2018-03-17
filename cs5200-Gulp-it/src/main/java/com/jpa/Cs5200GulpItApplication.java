92e3320014dc32a8dc6503219a1a5508427da655
Aman Rayat <rayat.a@husky.neu.edu>
2018-03-17 15:31:57 -0400

    setting up the project

---
+++ cs5200-Gulp-it/src/main/java/com/jpa/Cs5200GulpItApplication.java
@@ -0,0 +1,12 @@
+package com.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5200GulpItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200GulpItApplication.class, args);
+	}
+}
