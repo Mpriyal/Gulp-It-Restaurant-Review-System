55a0f4598ad9a2b066fbdc4f092dceabed416e7f
Aman Rayat <rayat.a@husky.neu.edu>
2018-03-21 04:08:40 -0400

    adding few changes

--- cs5200-Gulp-it/src/main/java/com/jpa/dao/MenuDao.java
+++ cs5200-Gulp-it/src/main/java/com/jpa/dao/MenuDao.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.jpa.models.Menu;
-import com.jpa.models.Restaurant;
 
 public class MenuDao {
 	final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
@@ -67,6 +66,37 @@ public class MenuDao {
 			String AllMenus = "SELECT * FROM Menu WHERE name=?";
 			statement= conn.prepareStatement(AllMenus);
 			statement.setInt(1, menuName);
+			resultset = statement.executeQuery();
+			while(resultset.next()) {
+				int id= resultset.getInt("id");
+				String name = resultset.getString("name");
+				int price = resultset.getInt("price");
+				String description = resultset.getString("description");
+				int restaurant1 = resultset.getInt("Restaurant");
+				Menu menu = new Menu(id,name,price,description,restaurant1);
+				menus.add(menu);
+			}
+			statement.close();
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return menus;
+	}
+	
+	public List<Menu> findAllMenuByRestaurant (int RestaurantId) {
+		List <Menu> menus = new ArrayList<>();
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			String AllMenusByRestaurant = "SELECT * FROM Menu WHERE Restaurant=?";
+			statement= conn.prepareStatement(AllMenusByRestaurant);
+			statement.setInt(1, RestaurantId);
 			resultset = statement.executeQuery();
 			while(resultset.next()) {
 				int id= resultset.getInt("id");
