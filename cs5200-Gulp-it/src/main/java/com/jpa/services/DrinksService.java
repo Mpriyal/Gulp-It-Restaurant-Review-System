d2b39117ea05aecb305d3f58280db243a7b27df5
Aman Rayat <rayat.a@husky.neu.edu>
2018-03-29 23:05:59 -0400

    adding few changes

--- cs5200-Gulp-it/src/main/java/com/jpa/services/DrinksService.java
+++ cs5200-Gulp-it/src/main/java/com/jpa/services/DrinksService.java
@@ -1,5 +1,28 @@
 package com.jpa.services;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpa.dao.DrinksDao;
+import com.jpa.models.Drinks;
+
+@RestController
 public class DrinksService {
 
+	@RequestMapping(value= "api/restaurant/{RestaurantId}/drinks", method = RequestMethod.GET)
+	public List<Drinks> findAllDrinksForRestaurant(@PathVariable(name="RestaurantId") int RestaurantId) {
+		DrinksDao drinksDao = DrinksDao.getInstance();
+		return drinksDao.findAllDrinksByRestaurant(RestaurantId);
+	}
+	
+	@RequestMapping(value="/api/owner/{OwnerId}/restaurant/{RestaurantId}/drinks", method=RequestMethod.POST)
+	public int addDrinksForRestaurant(@RequestBody Drinks drinks,@PathVariable(name="RestaurantId") int RestaurantId) {
+		DrinksDao dao = DrinksDao.getInstance();
+		return dao.addDrinksForRestaurant(drinks, RestaurantId);
+	}
 }
--- cs5200-Gulp-it/src/main/java/com/jpa/services/FoodService.java
+++ cs5200-Gulp-it/src/main/java/com/jpa/services/FoodService.java
@@ -13,10 +13,27 @@ import com.jpa.models.Food;
 @RestController
 public class FoodService {
 
+	/**
+	 * function for getting all food for a particular restaurant
+	 * @param RestaurantId
+	 * @return
+	 */
 	@RequestMapping(value= "api/restaurant/{RestaurantId}/food", method = RequestMethod.GET)
 	public List<Food> findAllFoodForRestaurant(@PathVariable(name="RestaurantId") int RestaurantId) {
 		FoodDao foodDao = FoodDao.getInstance();
 		return foodDao.findAllFoodByRestaurant(RestaurantId);
+	}
+	
+	/**
+	 * finding food for a restaurant for a certain id 
+	 * @param foodId
+	 * @return
+	 */
+	@RequestMapping(value= "api/restaurant/{RestaurantId}/food/{foodName}", method = RequestMethod.GET)
+	public List<Food> findFoodForRestaurant( @PathVariable(name="foodName") String foodName,  @PathVariable(name="RestaurantId") int RestaurantId)  
+	{
+		FoodDao foodDao = FoodDao.getInstance();
+		return foodDao.findFoodByNameForRestaurant(foodName, RestaurantId);
 	}
 	
 	@RequestMapping(value="/api/owner/{OwnerId}/restaurant/{RestaurantId}/food", method=RequestMethod.POST)
